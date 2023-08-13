e7a04a819a6f124f649f7727274471d9bbea157f
gfnvvlntn <gfnvvlntn@gmail.com>
2023-08-13 15:38:26 +0300

    feat(back): add secret token

--- src/service/token-service.ts
+++ src/service/token-service.ts
@@ -2,10 +2,12 @@ import jwt from 'jsonwebtoken'
 import TokenModel from '../models/token-model'
 import IToken from "../interfaces/token";
 
+const config = require('config');
+
 class TokenService {
     generateTokens(payload): { accessToken: string, refreshToken: string } {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN, {expiresIn: '15m'})
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN, {expiresIn: '30d'})
+        const accessToken = jwt.sign(payload, config.get('jwtAccessToken'), {expiresIn: '15m'})
+        const refreshToken = jwt.sign(payload, config.get('jwtRefreshToken'), {expiresIn: '30d'})
 
         return {
             accessToken,
@@ -15,7 +17,7 @@ class TokenService {
 
     validateAccessToken(token) {
         try {
-            return jwt.verify(token, process.env.JWT_ACCESS_TOKEN)
+            return jwt.verify(token, config.get('jwtAccessToken'))
 
         } catch (e) {
             return null
@@ -24,7 +26,7 @@ class TokenService {
 
     validateRefreshToken(token) {
         try {
-            return jwt.verify(token, process.env.JWT_REFRESH_TOKEN)
+            return jwt.verify(token, config.get('jwtRefreshToken'))
         } catch (e) {
             return null
         }
